113aaf8d3364a5057e2f553d51f27f4b76cef59f
mrrat100 <myfamilygud@gmail.com>
2024-10-17 17:23:53 +0300

    Customeruser model were created

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser, PermissionsMixin
+
 
 # Create your models here.
+
+class Customeruser(AbstractUser, PermissionsMixin):
+    class Role(models.TextChoices):
+        ADMIN = 'admin', 'Admin'
+        CUSTOMER = 'customer', 'Customer'
+        TEACHER = 'teacher', 'Teacher'
+        STUDENT = 'student', 'Student'
+        GUEST = 'guest', 'Guest'
+    
+    base_role   = Role.ADMIN
+
+    role        = models.CharField(max_length=50, choices=Role.choices)
+
+    def save(self, *args, **kwargs):
+        if not self.pk:
+            self.role = self.base_role
+            return super().save(*args, **kwargs)  
